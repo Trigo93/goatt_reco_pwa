966fd60a587eb6a39b7198cb149f17585b935211
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2023-01-22 21:47:26 +0100

    Update prompt.js

--- static/prompt.js
+++ static/prompt.js
@@ -1,16 +1,17 @@
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', function(event) {
+window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  document.getElementById("pwaa").style.display = 'block';
 });
 
-// Installation must be done by a user gesture! Here, the button click
-btnAdd.addEventListener('click', (e) => {
+document.getElementById("pwaa").addEventListener('click', (e) => {
   // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
+  document.getElementById("pwaa").style.display = 'none';
   // Show the prompt
   deferredPrompt.prompt();
   // Wait for the user to respond to the prompt
