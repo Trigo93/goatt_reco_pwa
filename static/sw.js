fd6475b7d5ed82b8652eb5028b93895eedda2a3e
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2023-01-24 22:31:46 +0100

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,13 +1,48 @@
-// sw.js
+console.log('Hello from sw.js');
 
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ...', event);
-});
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
 
-self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ...', event);
-});
+  workbox.precaching.precacheAndRoute([
+    {
+      "url": "/",
+      "revision": "1"
+    }
+  ]);
+
+  workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'static-resources',
+    }),
+  );
+
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    }),
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'googleapis',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30,
+        }),
+      ],
+    }),
+  );
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
