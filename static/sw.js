13dd8e808e7a537e947f9f9cb92e0b4203974fcd
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2023-01-24 23:07:48 +0100

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,48 +1,20 @@
-console.log('Hello from sw.js');
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
-
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-
-  workbox.precaching.precacheAndRoute([
-    {
-      "url": "/",
-      "revision": "1"
-    }
-  ]);
-
-  workbox.routing.registerRoute(
-    /\.(?:js|css)$/,
-    workbox.strategies.staleWhileRevalidate({
-      cacheName: 'static-resources',
-    }),
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/static/css/form.css',
+        '/static/css/result.css',
+        '/static/icons/app.png',
+      ]);
+    })
   );
+});
 
-  workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|svg)$/,
-    workbox.strategies.cacheFirst({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        }),
-      ],
-    }),
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
-
-  workbox.routing.registerRoute(
-    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-    workbox.strategies.cacheFirst({
-      cacheName: 'googleapis',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 30,
-        }),
-      ],
-    }),
-  );
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+});
