a86ca48eb0163f460583334b68aac16e52506bba
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2023-01-25 01:38:40 +0100

    Update register.js

--- static/register.js
+++ static/register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register(href = '/sw.js', { scope: '/' }).then(function(registration) {
+      navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(registration) {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
         console.log('ServiceWorker registration failed: ', err);
